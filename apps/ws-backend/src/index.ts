1ee6f91b5f8c1d408415c887d9fef04463e8bcb2
Amanrathi07 <amanrathi4321@gmail.com>
2025-12-02 01:44:29 +0530

    ws working

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,17 +1,47 @@
-import { WebSocketServer } from "ws"
-import jwt from "jsonwebtoken" 
-
+import { WebSocketServer,WebSocket } from "ws"
+import jwt, { JwtPayload } from "jsonwebtoken" 
 const wss = new WebSocketServer({port:8080})
 
 
-wss.on("connection",(ws)=>{
+import dotenv from "dotenv";
 
-    ws.on("message",(chat)=>{
-        ws.send("okk");
-        console.log(chat.toString())
-        
+dotenv.config({
+    path:"../../.env"
+})
+
+
+
+async function checkUser(token:string){
+    const decode = await jwt.verify(token,process.env.SECRET!);
+    return decode
+}
+
+interface users{
+    userId : string | JwtPayload,
+    rooms : [] ,
+    ws : WebSocket
+}
+
+const users:users[] = []
+
+wss.on("connection",(ws , Request:Request)=>{
+
+    const url = Request.url
+    if(!url) return ;
+
+    const queryParms =new URLSearchParams(url.split('?')[1])
+    const token = queryParms.get('token')|| "";
+    const userId = checkUser(token)
+    if(!userId){
+        ws.close()
+        return null ;
+    }
+
+    users.push({
+        userId  ,
+        rooms : [],
+        ws 
     })
 
-    
 })
 
